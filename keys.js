6c2627690b904b1d8c78f60a28c2a12aa58679dc
amleshkumar0022 <amleshkumar0022@gmail.com>
2024-03-09 21:46:58 +0530

    Initial commit

---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoUrl: "mongodb+srv://amleshkumar0022:Amlesh123@cluster0.lppkknd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    Jwt_secret: "faslkfocvneofu"
+
+};
